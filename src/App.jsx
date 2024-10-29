484ddb23e7617e9c2aca38feff4ae32b05fd67e1
Sunil Thapa <sunilthapa056@outlook.com>
2024-10-29 11:28:55 +0545

    project start

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className="main-container">
+        <h1>Tic Tac Toe</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
